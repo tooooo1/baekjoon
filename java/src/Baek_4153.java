ea9506bc0e8992030afec60c4c967788fc5f7078
tooooo1 <wer4272@nate.com>
2021-04-15 05:07:50 +0900

    study

---
+++ java/src/Baek_4153.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Baek_4153 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        while(true) {
+
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int z = in.nextInt();
+
+            if(x == 0 && y == 0 && z == 0) break;
+
+            if((x * x + y * y) == z * z) {
+                System.out.println("right");
+            } else if(x * x == (y * y + z * z)) {
+                System.out.println("right");
+            } else if(y * y == (z * z + x * x)) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+
+        }
+
+    }
+}
