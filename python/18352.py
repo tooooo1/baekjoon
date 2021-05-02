fdfe6a161b123a1d55fbe9188aa334c8bdccaa59
tooooo1 <wer4272@nate.com>
2021-05-03 02:00:18 +0900

    study

--- python/18352.py
+++ python/18352.py
@@ -1,4 +1,7 @@
+import sys
 from collections import deque
+input = sys.stdin.readline()
+
 
 def bfs():
     q = deque()
@@ -8,13 +11,14 @@ def bfs():
         if x == k:
             print(dist[x])
             break
-        for nx in (x-1,x+1,x*2):
-            if 0<= nx <= MAX and not dist[nx]:
-                dist[nx] = dist[x] +1
-                q.append(nx)
+
+        for j in (x-1,x+1,x*2):
+            if 0<= j <= MAX and not dist[j]:
+                dist[j] = dist[x] +1
+                q.append(j)
 
 MAX = 100000
-n,k = map(int,input().split())
+n,k = map(int,input.split())
 dist = [0] * (MAX+1)
 
 bfs()
