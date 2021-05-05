9f898ee2e740119a4518c57ba2a908a7b7fe97b2
tooooo1 <wer4272@nate.com>
2021-05-06 03:13:19 +0900

    study

--- cpp/2941.cpp
+++ cpp/2941.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    vector<string> v = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+    int a;
+    string input;
+    cin >> input;
+    
+    for (int i =0; i<8; i++) {
+        a = input.find(v[i]);
+        for (; a != string::npos; a = input.find(v[i])) {
+            input.replace(a, v[i].length(),"@");
+        }
+    }
 
-}
-
-char change(char input) {
-    for (int i =0; ;i++)
+    cout << input.length();
 }
