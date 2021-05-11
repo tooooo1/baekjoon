9d10275cb826d20d7e95e9b7036c3deacf5d3fb4
tooooo1 <wer4272@nate.com>
2021-05-11 20:25:56 +0900

    study

--- cpp/2110.cpp
+++ cpp/2110.cpp
@@ -8,19 +8,34 @@ int n,c;
 vector<int> x;
 
 int main() {
-    cin >> n>> c;
-
-    for (auto i =0; i<n;i++) {
+    cin >>n>>c;
+    for(auto i =0; i<n;i++) {
         int a;
         cin >> a;
         x.push_back(a);
     }
     sort(x.begin(),x.end());
 
-    int start =0;
-    int end = *max_element(x.begin(),x.end());
+    int start=0;
+    int end=x[n-1];
+    int result=0;
+    int temp,count;
 
     while(start<=end) {
-        
+        int mid = (start+end)/2;
+        temp=0, count =1;
+        for(auto i=1;i<n;i++) {
+            if(x[i]-x[temp]>=mid) {
+                temp=i;
+                count++;
+            }
+        }
+        if(count>=c) {
+            result=mid;
+            start=mid+1;
+        }else {
+            end=mid-1;
+        }
     }
+    cout <<result;
 }
