a1d39595ed70d807c996ad20423d4e21415d1d42
tooooo1 <wer4272@nate.com>
2021-08-01 16:19:27 +0900

    study

---
+++ cpp/11654.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char x;
+    cin >> x;
+    int result=x;
+    cout <<result;
+}
--- cpp/18513.cpp
+++ cpp/18513.cpp
@@ -11,23 +11,23 @@ int dx[]={1,-1};
 queue<int> q;
 set<int> s;
 
-long bfs() {
+ll bfs() {
     while(!q.empty()) {
-        int Size = q.size();
-        for(int i=0; i<Size;i++) {
+        int Size = q.size(); //size가 계속 변할 수 있으므로 고정해준다
+        for(int i=0; i<Size;i++) { //size만큼 반복
             int a = q.front();
             q.pop();
-            for(int i=0;i<2;i++) {
-                int nx = a + dx[i];
-                if(!s.count(nx)) {
-                    s.insert(nx); q.push(nx);
-                    result+=misfortune;
+            for(int j=0;j<2;j++) {
+                int nx = a + dx[j]; //좌우 탐색
+                if(!s.count(nx)) { //값이 없다면
+                    s.insert(nx); q.push(nx);//추가
+                    result+=misfortune;//불행도 갱신
 
-                    if(!--K) return result;
+                    if(!--K) return result;//모든 집을 지었다면 종료
                 }
             }
         }
-        misfortune++;
+        misfortune++; //동일한 거리 다 했으니 1 증가 후 증가한만큼의 거리, 다시 탐색
     }
     return result;
 }
