f81f2037d387f7fefc03a6525a0949685f792045
tooooo1 <wer4272@nate.com>
2021-08-31 20:46:03 +0900

    study

---
+++ python/4673.py
@@ -0,0 +1,13 @@
+numbers = set(range(1,10001))
+
+notSelfNumbers = set()
+
+for i in range(1,10001):
+    for j in str(i): #문자열로하여 인덱스 접근
+        i += int(j)
+    notSelfNumbers.add(i)
+
+selfNum = numbers - notSelfNumbers
+
+for i in sorted(selfNum):
+    print(i)
