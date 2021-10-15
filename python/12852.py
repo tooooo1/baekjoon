fa541e5fe0298b05ae7137b649fd9f2a04232cda
tooooo1 <wer4272@nate.com>
2021-10-15 21:48:23 +0900

    study

--- python/12852.py
+++ python/12852.py
@@ -1,23 +1,21 @@
-import sys
-
-input = sys.stdin.readline
-
 N = int(input())
 
-X = [0 for _ in range(N+1)]
+result = [[0, []] for _ in range(N + 1)]
+result[1][0] = 0 
+result[1][1] = [1] 
 
-for i in range(1, N+1):
-    if i == 1:
-        X[i] == 0
-        continue
-    
-    X[i] = X[i-1] + 1
+for i in range(2, N + 1):
+    result[i][0] = result[i-1][0] + 1
+    result[i][1] = result[i-1][1] + [i]
+     
+    if i % 3 == 0 and result[i//3][0] + 1 < result[i][0]:
+        result[i][0] = result[i//3][0] + 1
+        result[i][1] = result[i//3][1] + [i]
 
-    if i % 3 == 0 and X[i//3] + 1 < X[i]:
-        X[i] = X[i//3] + 1
-    if i % 2 == 0 and X[i//2] + 1< X[i]:
-        X[i] = X[i//2] + 1
-
-print(X[N])
-for i in range(N):
-    print(q[i], end=' ')
+    if i % 2 == 0 and result[i//2][0] + 1 < result[i][0]:
+        result[i][0] = result[i//2][0] + 1
+        result[i][1] = result[i//2][1] + [i]
+        
+print(result[N][0])
+for i in result[N][1][::-1]:
+    print(i, end=' ')
